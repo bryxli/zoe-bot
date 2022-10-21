ba73c15e6ead2c10faa31dc9d62746f629f6a661
bryxli <bryanzero765@gmail.com>
2022-10-21 12:19:16 -0500

    router, home page

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
 
 function App() {
   return (
-    <></>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
