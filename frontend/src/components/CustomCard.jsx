de281c4a53f12e5cd97b7c8ac4c31e000578e3db
bryxli <bryanzero765@gmail.com>
2022-10-21 21:20:29 -0500

    recent table

--- frontend/src/components/CustomCard.jsx
+++ frontend/src/components/CustomCard.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
 
 const CustomCard = (detail) => {
-    return(
-        <Card>
-            <Card.Body>
-            <Card.Text>
-                {detail["detail"]}
-            </Card.Text>
-            </Card.Body>
-        </Card>
-    );
-}
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Text>{detail["detail"]}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default CustomCard;
