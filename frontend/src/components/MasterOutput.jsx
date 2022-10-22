4208b1297baaf42533f5f8a4edec22ccf6461429
bryxli <bryanzero765@gmail.com>
2022-10-21 21:48:37 -0500

    connect flask with react final

--- frontend/src/components/MasterOutput.jsx
+++ frontend/src/components/MasterOutput.jsx
@@ -1,31 +1,29 @@
 import React, { useEffect, useState } from "react";
-import CustomCard from './CustomCard'
+import CustomCard from "./CustomCard";
 
 const MasterOutput = () => {
-  const [gameList, setGameList] = useState(0)
+  const [gameList, setGameList] = useState(0);
 
   useEffect(() => {
-    fetch("/data").then(response =>
-      response.json().then(data => {
+    fetch("/data").then((response) =>
+      response.json().then((data) => {
         setGameList(data.games);
       })
     );
   }, []);
 
   const games = Array.from(gameList);
-  var cards = []
+  var cards = [];
   for (const game of games) {
-    const card = <CustomCard detail={game} key={game}/>;
-    cards.push(card)
+    const card = <CustomCard detail={game} key={game} />;
+    cards.push(card);
   }
 
   return (
     <div>
-      <ul>
-        {cards}
-      </ul>
+      <ul className="customCard">{cards}</ul>
     </div>
   );
-}
+};
 
 export default MasterOutput;
