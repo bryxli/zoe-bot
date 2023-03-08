a854e7b814c738b3eab0630bd40711635432506f
bryxli <bryanzero765@gmail.com>
2023-03-08 13:21:41 -0600

    finish cass functions

--- src/cass_wrapper.py
+++ src/cass_wrapper.py
@@ -8,9 +8,24 @@ with open('config.json') as file:
 cass.set_riot_api_key(config['riot_key'])
 
 def find_player_by_name(name, region):
-    try:
-        summoner = cass.get_summoner(name=name, region=region)
-        summoner.account_id
+    summoner = cass.get_summoner(name=name, region=region)
+    if (summoner.exists):
         return summoner
-    except Exception as e:
-        return e
+    return None
+
+def find_player_by_accountid(accountid, region):
+    summoner = cass.get_summoner(account_id=accountid, region=region)
+    if(summoner.exists):
+        return summoner
+    return None
+
+def find_most_recent_match(summoner):
+    match_history = summoner.match_history
+    if (match_history.count > 0):
+        return match_history[0]
+    return None
+
+def find_participant_id(summoner, match):
+    for participant in match.participants:
+        if participant.summoner == summoner:
+            return participant.id
