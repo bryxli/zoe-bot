08a428592a9f0be4aca1003dd5d18cf836dacd02
bryxli <bryanzero765@gmail.com>
2023-03-07 20:14:48 -0600

    bot running

--- src/app.py
+++ src/app.py
@@ -8,15 +8,15 @@ with open('config.json') as file:
     config = json.load(file)
 
 intents = discord.Intents.all()
-bot = Bot(command_prefix=commands.when_mentioned_or(config.prefix), intents=intents, help_command=None)
+bot = Bot(command_prefix=commands.when_mentioned_or(config['prefix']), intents=intents, help_command=None)
 bot.config = config
 
 @bot.event
 async def on_ready() -> None:
-    await bot.change_presence(activity=discord.Game("?help"))
+    await bot.change_presence(activity=discord.Game("#help"))
 
 @bot.command()
 async def help(ctx):
     await ctx.send('help')
 
-bot.run(config.token)
+bot.run(config['token'])
