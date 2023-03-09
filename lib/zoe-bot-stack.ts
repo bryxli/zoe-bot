d6a889923781af0d1d8c10d10814c2536a30e761
bryxli <bryanzero765@gmail.com>
2023-03-09 13:11:09 -0600

    v1

---
+++ lib/zoe-bot-stack.ts
@@ -0,0 +1,39 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export class ZoeBotStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'ZoeBotVpc', {
+      natGateways: 0,
+    });
+
+    const dynamo = new dynamodb.Table(this, 'ZoeBotTable', {
+      partitionKey: { name: 'guild_id', type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, 'ZoeBotSg', {
+      vpc: vpc,
+      securityGroupName: 'ZoeBotSg',
+    });
+
+    const iamRole = new iam.Role(this, 'ZoeBotIam', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    });
+    iamRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+    dynamo.grantReadWriteData(iamRole);
+
+    const ec2Instance = new ec2.Instance(this,'ZoeBotInstance', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
+      vpc: vpc,
+      securityGroup: securityGroup,
+      role: iamRole
+    });
+  }
+}
