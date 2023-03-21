905e2d333642ec53d2d0746774911f9ab72936d1
bryxli <bryanzero765@gmail.com>
2023-03-20 22:23:36 -0500

    comments

--- lib/zoe-bot-stack.ts
+++ lib/zoe-bot-stack.ts
@@ -8,15 +8,18 @@ export class ZoeBotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create DynamoDB table
     const table = new dynamodb.Table(this, 'ZoeBotTable', {
       partitionKey: { name: 'guild_id', type: dynamodb.AttributeType.NUMBER },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
+    // Create EC2 role
     const iamRole = new iam.Role(this, 'ZoeBotIam', {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
     });
 
+    // Create policy to access DynamoDB table
     const dynamoDbPolicy = new iam.ManagedPolicy(this, 'ZoeBotPolicy', {
       statements: [
         new iam.PolicyStatement({
@@ -33,9 +36,11 @@ export class ZoeBotStack extends cdk.Stack {
       ]
     });
 
+    // Attach SSM and DynamoDB access to EC2 role
     iamRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
     iamRole.addManagedPolicy(dynamoDbPolicy);
 
+    // Create VPC
     const vpc = new ec2.Vpc(this, 'ZoeBotVpc', {
       natGateways: 0,
       subnetConfiguration: [
@@ -46,18 +51,20 @@ export class ZoeBotStack extends cdk.Stack {
       ],
     });
 
+    // Create EC2 security group
     const securityGroup = new ec2.SecurityGroup(this, 'ZoeBotSg', {
       vpc: vpc,
       securityGroupName: 'ZoeBotSg',
     });
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
 
-    /*
+    /* 
     const key = new ec2.CfnKeyPair(this, 'ZoeKeyPair', {
       keyName: 'ZoeKey',
     });
     */
 
+    // Create EC2 instance
     const ec2Instance = new ec2.Instance(this,'ZoeBotInstance', {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       machineImage: new ec2.AmazonLinuxImage(),
@@ -67,6 +74,7 @@ export class ZoeBotStack extends cdk.Stack {
       // keyName: 'ZoeKey',
     });
 
+    // Export EC2 instance id
     new cdk.CfnOutput(this, 'id', { value: ec2Instance.instanceId });
   }
 }
