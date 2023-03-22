2269eab3d0fd59b4aa7d273fd708e55d2317c21c
bryxli <bryanzero765@gmail.com>
2023-03-22 09:48:12 -0500

    node

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 sudo yum update -y
 sudo yum groupinstall "Development Tools" -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 16
 sudo yum install python38-pip -y
 sudo python3 -m pip install --upgrade pip
 cd /home/ssm-user
