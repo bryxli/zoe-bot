9db1059e7387a6bb1657121c2cf95592acd25d3c
bryxli <bryanzero765@gmail.com>
2023-03-22 10:53:19 -0500

    streamline userdata

--- lib/zoe-bot-stack.ts
+++ lib/zoe-bot-stack.ts
@@ -63,6 +63,11 @@ export class ZoeBotStack extends cdk.Stack {
     });
     securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
 
+    // Create userData
+    const userDataScript = fs.readFileSync('./startup.sh', 'utf8');
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(userDataScript);
+
     // Create EC2 instance
     const ec2Instance = new ec2.Instance(this,'ZoeBotInstance', {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
@@ -70,11 +75,8 @@ export class ZoeBotStack extends cdk.Stack {
       vpc: vpc,
       securityGroup: securityGroup,
       role: iamRole,
-      userData: ec2.UserData.forLinux(),
+      userData: userData,
     });
-
-    const userDataScript = fs.readFileSync('./startup.sh', 'utf8');
-    ec2Instance.userData.addCommands(userDataScript);
 
     // Export EC2 instance id
     new cdk.CfnOutput(this, 'id', { value: ec2Instance.instanceId });
