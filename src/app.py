ef900626c7ab5d4720ee86d3a9d5d9d73831582f
bryxli <bryanzero765@gmail.com>
2023-05-28 01:17:03 -0500

    exception for edge cases (discord guild / channel or summoner account id does not exist)

--- src/app.py
+++ src/app.py
@@ -36,44 +36,47 @@ async def loop():
         channel_id = guild['channel_id']['N']
         print(f'checking in {guild_id}:{channel_id}')
 
-        discord_guild = bot.get_guild(int(guild_id))
-        discord_channel = discord_guild.get_channel(int(channel_id))
+        try:
+            discord_guild = bot.get_guild(int(guild_id))
+            discord_channel = discord_guild.get_channel(int(channel_id))
 
-        # [{'M':{account_id:{'S':last_created}}}...]
-        for user_data in guild['userlist']['L']:
-            account_id = list(user_data['M'].keys())[0]
-            print(f'found user {account_id}')
+            # [{'M':{account_id:{'S':last_created}}}...]
+            for user_data in guild['userlist']['L']:
+                account_id = list(user_data['M'].keys())[0]
+                print(f'found user {account_id}')
 
-            summoner = cass.find_player_by_accountid(
-                account_id, guild['region']['S'])
+                summoner = cass.find_player_by_accountid(
+                    account_id, guild['region']['S'])
 
-            match_history = summoner.match_history
-            if (match_history.count > 0):
-                match = match_history[0]
-                for participant in match.participants:
-                    if participant.summoner.account_id == summoner.account_id:
-                        id = match.participants.index(participant)
-                        break
+                match_history = summoner.match_history
+                if (match_history.count > 0):
+                    match = match_history[0]
+                    for participant in match.participants:
+                        if participant.summoner.account_id == summoner.account_id:
+                            id = match.participants.index(participant)
+                            break
 
-                last_created_old = user_data['M'][account_id]['S']
-                last_created = str(match.creation)
-                if last_created != last_created_old:
-                    print('found new match')
+                    last_created_old = user_data['M'][account_id]['S']
+                    last_created = str(match.creation)
+                    if last_created != last_created_old:
+                        print('found new match')
 
-                    player = match.participants[id]
-                    summoner_name = summoner.name
-                    champion_name = player.champion.name
-                    kda = str(round(player.stats.kda, 2))
-                    win = player.stats.win
+                        player = match.participants[id]
+                        summoner_name = summoner.name
+                        champion_name = player.champion.name
+                        kda = str(round(player.stats.kda, 2))
+                        win = player.stats.win
 
-                    if win:
-                        t = Template(random.choice(template['win']))
-                    else:
-                        t = Template(random.choice(template['lose']))
+                        if win:
+                            t = Template(random.choice(template['win']))
+                        else:
+                            t = Template(random.choice(template['lose']))
 
-                    db.update_user(guild_id, account_id, last_created)
+                        db.update_user(guild_id, account_id, last_created)
 
-                    await discord_channel.send(t.substitute(summoner_name=summoner_name, kda=kda, champion_name=champion_name))
+                        await discord_channel.send(t.substitute(summoner_name=summoner_name, kda=kda, champion_name=champion_name))
+        except Exception as e:
+            print(e)
 
 
 @bot.command(description='zoe will talk to you')
