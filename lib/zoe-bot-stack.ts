83a1fd7396bcb000b6695384b9dda5ea80a6fbc2
bryxli <bryanzero765@gmail.com>
2023-09-16 14:01:54 -0500

    lambda cdk

--- lib/zoe-bot-stack.ts
+++ lib/zoe-bot-stack.ts
@@ -1,84 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as fs from 'fs'
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class ZoeBotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create DynamoDB table
-    const table = new dynamodb.Table(this, 'ZoeBotTable', {
-      partitionKey: { name: 'guild_id', type: dynamodb.AttributeType.NUMBER },
-      // removalPolicy: cdk.RemovalPolicy.DESTROY,
-      tableName: 'ZoeBotTable'
-    });
-
-    // Create EC2 role
-    const iamRole = new iam.Role(this, 'ZoeBotIam', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
-    });
-
-    // Create policy to access DynamoDB table
-    const dynamoDbPolicy = new iam.ManagedPolicy(this, 'ZoeBotPolicy', {
-      statements: [
-        new iam.PolicyStatement({
-          actions: [
-            'dynamodb:Scan',
-            'dynamodb:GetItem',
-            'dynamodb:PutItem',
-            'dynamodb:DeleteItem',
-            'dynamodb:UpdateItem',
-          ],
-          resources: [
-            table.tableArn
-          ]
-        })
-      ]
-    });
-
-    // Attach SSM and DynamoDB access to EC2 role
-    iamRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
-    iamRole.addManagedPolicy(dynamoDbPolicy);
-
-    // Create VPC
-    const vpc = new ec2.Vpc(this, 'ZoeBotVpc', {
-      natGateways: 0,
-      subnetConfiguration: [
-        {
-          name: 'public',
-          subnetType: ec2.SubnetType.PUBLIC,
+    const dockerFunction = new lambda.DockerImageFunction(
+      this,
+      "ZoeFunction",
+      {
+        code: lambda.DockerImageCode.fromImageAsset("./src"),
+        memorySize: 1024,
+        timeout: cdk.Duration.seconds(10),
+        architecture: lambda.Architecture.X86_64,
+        environment: {
+          DISCORD_PUBLIC_KEY: "fde7a11dc5c641dc8d51fada6f7d1f43055a5ac223b16d657fa92dc86b1600dc",
         },
-      ],
+      }
+    );
+
+    const ZoeUrl = dockerFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
     });
 
-    // Create EC2 security group
-    const securityGroup = new ec2.SecurityGroup(this, 'ZoeBotSg', {
-      vpc: vpc,
-      securityGroupName: 'ZoeBotSg',
+    new cdk.CfnOutput(this, "ZoeUrl", {
+      value: ZoeUrl.url,
     });
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
-
-    // Create userData
-    const userDataScript = fs.readFileSync('./startup.sh', 'utf8');
-    const userData = ec2.UserData.forLinux();
-    userData.addCommands(userDataScript);
-
-    // Create EC2 instance
-    const ec2Instance = new ec2.Instance(this,'ZoeBotInstance', {
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: new ec2.AmazonLinuxImage(),
-      vpc: vpc,
-      securityGroup: securityGroup,
-      role: iamRole,
-      userData: userData,
-    });
-
-    // Export EC2 instance id
-    new cdk.CfnOutput(this, 'id', { value: ec2Instance.instanceId });
   }
 }
