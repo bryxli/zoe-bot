cc79da32a4709cbc2cd5d92cb73ea5bfc57447f1
bryxli <bryanzero765@gmail.com>
2023-09-21 01:39:26 -0500

    rate limit and auto-retry

--- src/upload_commands/register_commands.py
+++ src/upload_commands/register_commands.py
@@ -4,14 +4,15 @@
 import requests
 import yaml
 import os
+import time
 
 
 TOKEN = os.environ.get("TOKEN")
+TOKEN = 'MTA4Mjg0NjIyODY3MDMyNDc4Ng.GBJoxJ.M7u83GfB5C3-XRgqOonX2eGwkRId3xBNxZfysc'
 APPLICATION_ID = os.environ.get("APPLICATION_ID")
-GUILD_ID = os.environ.get("GUILD_ID")
+APPLICATION_ID= '1082846228670324786'
 
 URL = f"https://discord.com/api/v9/applications/{APPLICATION_ID}/commands"
-URL_TEST = f"https://discord.com/api/v9/applications/{APPLICATION_ID}/guilds/{GUILD_ID}/commands"
 
 
 with open("discord_commands.yaml", "r") as file:
@@ -20,8 +21,27 @@ with open("discord_commands.yaml", "r") as file:
 commands = yaml.safe_load(yaml_content)
 headers = {"Authorization": f"Bot {TOKEN}", "Content-Type": "application/json"}
 
-# Send the POST request for each command
-for command in commands:
+
+failed = []
+
+
+def upload_command(command):
+    global failed
     response = requests.post(URL, json=command, headers=headers)
     command_name = command["name"]
-    print(f"Command {command_name} created: {response.status_code}")
+        
+    if response.status_code == 201 or response.status_code == 200:
+        print(f"Command {command_name} created: {response.status_code}")
+    elif response.status_code == 429:
+        failed.append(command)
+        # Rate limited, sleep and retry after the reset time
+        print(f"Command {command_name} failed: {response.status_code} Pausing for 5 seconds...")
+        time.sleep(5)
+    else:
+        print(f"Failed to create command {command_name}: {response.status_code}")
+
+
+for command in commands:
+    upload_command(command)
+    if len(failed) > 0:
+        upload_command(failed.pop())
