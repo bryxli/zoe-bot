8ca2d1a5c78581f451980e295e0e7fd6c57ded39
bryxli <bryanzero765@gmail.com>
2023-10-19 18:24:02 -0500

    return user data

--- frontend/app/user/page.tsx
+++ frontend/app/user/page.tsx
@@ -1,6 +1,44 @@
-// TODO: display user details
-const User = () => {
-  return <>User!</>;
-};
+"use client";
 
-export default User;
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function User() {
+  const router = useRouter();
+  const [userInfo, setUserInfo] = useState({
+    username: "",
+    avatar: "",
+    id: "",
+  });
+
+  useEffect(() => {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [
+      fragment.get("access_token"),
+      fragment.get("token_type"),
+    ];
+
+    if (!accessToken) {
+      router.push("/");
+    }
+
+    fetch("https://discord.com/api/users/@me", {
+      headers: {
+        authorization: `${tokenType} ${accessToken}`,
+      },
+    })
+      .then((result) => result.json())
+      .then((response) => {
+        console.log(response);
+        const { username, avatar, id } = response;
+        setUserInfo({ username, avatar, id });
+      })
+      .catch(console.error);
+  }, [router]);
+
+  return (
+    <>
+      <h1>{userInfo.username}</h1>
+    </>
+  );
+}
