e7ec6b97b4e3f76dfd798b70724c4024c932cb0a
bryxli <bryanzero765@gmail.com>
2023-10-21 13:49:33 -0500

    duplicate type

--- frontend/app/contexts/AuthContext.tsx
+++ frontend/app/contexts/AuthContext.tsx
@@ -1,17 +1,11 @@
 "use client";
 
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode } from "react";
 
 import { UserProps } from "../types";
 
-interface SignInParams {
-  username: string;
-  avatar: string;
-  id: string;
-}
-
 interface AuthContextData {
-  signIn: (params: SignInParams) => void;
+  signIn: (params: UserProps) => void;
   signOut: () => void;
   userInfo: UserProps | null;
 }
@@ -25,7 +19,7 @@ export const AuthContext = createContext({} as AuthContextData);
 export function AuthProvider({ children }: AuthProviderProps) {
   const [userInfo, setUserInfo] = useState<UserProps | null>(null);
 
-  const signIn = ({ username, avatar, id }: SignInParams) =>
+  const signIn = ({ username, avatar, id }: UserProps) =>
     setUserInfo({ username, avatar, id });
 
   const signOut = () => setUserInfo(null);
