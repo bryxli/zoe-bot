feaf5e29e60d2a50b105e1d8204ec174e4fb43ab
bryxli <bryanzero765@gmail.com>
2023-10-21 14:56:11 -0500

    header in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { AuthProvider } from "./contexts/AuthContext";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Header />
+          {children}
+        </body>
       </AuthProvider>
     </html>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,9 @@
 // TODO: information
 // TODO: generated output
-import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
-      <Header />
       {/** Information */}
       {/** Output */}
     </>
