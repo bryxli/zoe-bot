ce32b015b13c5663763cf11b666bf7f672c6f265
bryxli <bryanzero765@gmail.com>
2023-12-01 13:00:30 -0600

    remove old cdk

--- src/bot/main/constants/env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-AWS_REGION = os.environ.get("SET_AWS_REGION")
-DISCORD_PUBLIC_KEY = os.environ.get("DISCORD_PUBLIC_KEY")
-RIOT_KEY = os.environ.get("RIOT_KEY")
-TOKEN = os.environ.get("TOKEN")
