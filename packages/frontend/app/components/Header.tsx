3f36e05e36d2a63ac26d1a4d35129aa585ba38b3
bryxli <bryanzero765@gmail.com>
2023-12-06 10:29:15 -0600

    logout

--- packages/frontend/app/components/Header.tsx
+++ packages/frontend/app/components/Header.tsx
@@ -4,10 +4,8 @@ import { useContext } from "react";
 import { Button, Container } from "react-bootstrap";
 
 import { AuthContext } from "../contexts/AuthContext";
-import { useRouter } from "next/navigation";
 
 export default function Header() {
-  const router = useRouter();
   const authContext = useContext(AuthContext);
   const { userInfo } = authContext;
 
@@ -15,28 +13,18 @@ export default function Header() {
   const redirect = "https://ddwebabika1cp.cloudfront.net"; // TODO: update readme for redirect instructions
   const href = `https://discord.com/api/oauth2/authorize?client_id=${application_id}&redirect_uri=${redirect}/load&response_type=token&scope=guilds%20identify`;
 
-  const handleLogout = async () => {
-    router.push("/");
-  };
-
-  const handleLogin = async () => {
-    router.push(application_id === undefined ? "#" : href);
-  }
-
   return (
     <Container className="d-flex flex-column align-items-center justify-content-center mb-4">
       <header className="font">zoe, the aspect of twighlight</header>
-      {
-        userInfo ? (
-          <Button className="authButton" onClick={handleLogout}>
-            Logout
-          </Button>
-        ) : (
-          <Button className="authButton" onClick={handleLogin}>
-            Login with Discord
-          </Button>
-        )
-      }
+      {userInfo ? (
+        <Button className="authButton" href="/logout">
+          Logout
+        </Button>
+      ) : (
+        <Button className="authButton" href={href}>
+          Login with Discord
+        </Button>
+      )}
     </Container>
   );
 }
