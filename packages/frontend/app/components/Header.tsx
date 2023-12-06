ef3cfced7daa3e4c07aca05c04916f4b244f05b4
bryxli <bryanzero765@gmail.com>
2023-12-05 23:54:27 -0600

    href

--- packages/frontend/app/components/Header.tsx
+++ packages/frontend/app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Button, Container } from "react-bootstrap";
 
 import { AuthContext } from "../contexts/AuthContext";
@@ -10,25 +10,14 @@ export default function Header() {
   const router = useRouter();
   const authContext = useContext(AuthContext);
   const { userInfo } = authContext;
-  const [applicationId, setApplicationId] = useState<string | undefined>(undefined);
 
+  const application_id = process.env.APPLICATION_ID;
   const redirect = "https://ddwebabika1cp.cloudfront.net"; // TODO: update readme for redirect instructions
-  const href = `https://discord.com/api/oauth2/authorize?client_id=${applicationId}&redirect_uri=${redirect}/load&response_type=token&scope=guilds%20identify`;
-
-  useEffect(() => {
-    setApplicationId(process.env.APPLICATION_ID);
-  }, [setApplicationId]);
+  const href = `https://discord.com/api/oauth2/authorize?client_id=${application_id}&redirect_uri=${redirect}/load&response_type=token&scope=guilds%20identify`;
 
   const handleLogout = async () => {
     router.push("/");
   };
-
-  const renderLoginButton = () => (
-    applicationId !== undefined && 
-      <Button className="authButton" href={href}>
-        Login with Discord
-      </Button>
-  );
 
   return (
     <Container className="d-flex flex-column align-items-center justify-content-center mb-4">
@@ -38,7 +27,11 @@ export default function Header() {
           <Button className="authButton" onClick={handleLogout}>
             Logout
           </Button>
-        ) : renderLoginButton()
+        ) : (
+          <Button className="authButton" href={application_id !== undefined ? href : "#"}>
+            Login with Discord
+          </Button>
+        )
       }
     </Container>
   );
