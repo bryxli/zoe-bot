5a2ae3699681fb5f63daa9c925b7bdce4094fa2a
bryxli <bryanzero765@gmail.com>
2023-12-05 23:33:01 -0600

    state and effect

--- packages/frontend/app/components/Header.tsx
+++ packages/frontend/app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
 
 import { AuthContext } from "../contexts/AuthContext";
@@ -10,17 +10,21 @@ export default function Header() {
   const router = useRouter();
   const authContext = useContext(AuthContext);
   const { userInfo } = authContext;
+  const [applicationId, setApplicationId] = useState<string | undefined>(undefined);
 
-  const application_id = process.env.APPLICATION_ID;
   const redirect = "https://ddwebabika1cp.cloudfront.net"; // TODO: update readme for redirect instructions
-  const href = `https://discord.com/api/oauth2/authorize?client_id=${application_id}&redirect_uri=${redirect}/load&response_type=token&scope=guilds%20identify`;
+  const href = `https://discord.com/api/oauth2/authorize?client_id=${applicationId}&redirect_uri=${redirect}/load&response_type=token&scope=guilds%20identify`;
+
+  useEffect(() => {
+    setApplicationId(process.env.APPLICATION_ID);
+  }, [setApplicationId]);
 
   const handleLogout = async () => {
     router.push("/");
   };
 
   const renderLoginButton = () => (
-    application_id !== undefined && 
+    applicationId !== undefined && 
       <Button className="authButton" href={href}>
         Login with Discord
       </Button>
@@ -29,11 +33,13 @@ export default function Header() {
   return (
     <Container className="d-flex flex-column align-items-center justify-content-center mb-4">
       <header className="font">zoe, the aspect of twighlight</header>
-      {userInfo ? (
-        <Button className="authButton" onClick={handleLogout}>
-          Logout
-        </Button>
-      ) : renderLoginButton()}
+      {
+        userInfo ? (
+          <Button className="authButton" onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : renderLoginButton()
+      }
     </Container>
   );
 }
