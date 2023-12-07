b747b03f147000aef90eb39df42dcf6e5cb8dce0
bryxli <bryanzero765@gmail.com>
2023-12-07 09:31:13 -0600

    deploy to dev

--- stacks/WebStack.ts
+++ stacks/WebStack.ts
@@ -1,17 +1,14 @@
 import { NextjsSite, StackContext, use } from "sst/constructs";
-import * as devConfig from "../configs/config-dev.json";
-import * as prodConfig from "../configs/config-prod.json";
+import * as config from "../configs/config.json";
 import { InfraStack } from "./InfraStack";
 
 export function WebStack({ app, stack }: StackContext) {
   const { table } = use(InfraStack);
 
-  const currentConfig = app.stage === "prod" ? prodConfig : devConfig;
-
   const site = new NextjsSite(stack, "frontend", {
     path: "packages/frontend",
     environment: {
-      APPLICATION_ID: currentConfig.application_id,
+      APPLICATION_ID: config.application_id,
     },
     bind: [table],
   });
