ae86a13c231c3ed888985350e443bf15b4810496
bryxli <bryanzero765@gmail.com>
2023-12-08 22:35:20 -0600

    refactor load to use api

--- packages/frontend/app/api/discord/guilds/route.ts
+++ packages/frontend/app/api/discord/guilds/route.ts
@@ -1,8 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to force-static
-export async function GET(request: NextRequest) {
-  return new NextResponse(
-    "calls discord user endpoint using params from request body",
-  );
+export async function POST(request: NextRequest) {
+  /*
+  fetch("https://discord.com/api/users/@me/guilds", {
+    headers: {
+      authorization: `${tokenType} ${accessToken}`,
+    },
+  })
+    .then((result) =>
+      result.status === 429 ? Promise.resolve("429") : result.json(),
+    )
+    .then((response) => {
+      if (response != 429) {
+        processGuilds(response);
+        router.push("/dashboard");
+      }
+    })
+    .catch(console.error);
+  */
+  return NextResponse.json(["test", "test2"]);
 }
--- packages/frontend/app/api/discord/user/route.ts
+++ packages/frontend/app/api/discord/user/route.ts
@@ -1,8 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to force-static
-export async function GET(request: NextRequest) {
-  return new NextResponse(
-    "calls discord guilds endpoint using params from request body",
-  );
+
+export async function POST(request: NextRequest) {
+  // return {username, avatar, id}
+  /*
+  fetch("https://discord.com/api/users/@me", {
+    headers: {
+      authorization: `${tokenType} ${accessToken}`,
+    },
+  })
+    .then((result) => result.json())
+    .then((response) => {
+      return response;
+    })
+    .catch(console.error);
+  */
+  return NextResponse.json({ username: "test", avatar: "test", id: "test" });
 }
