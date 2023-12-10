0d5c554822c1bdb9b171587c6465ec872ad1ad45
bryxli <bryanzero765@gmail.com>
2023-12-10 16:05:40 -0600

    remove deprecated domains

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.discordapp.com"],
+    remotePatterns: [
+      {
+        hostname: "cdn.discordapp.com",
+      },
+    ],
   },
 };
 
