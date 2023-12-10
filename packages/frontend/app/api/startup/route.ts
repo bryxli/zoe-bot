c137c60c7aa503dc8d543c850afc174c5fff2cb9
bryxli <bryanzero765@gmail.com>
2023-12-10 17:10:50 -0600

    warming function as post

--- packages/frontend/app/api/startup/route.ts
+++ packages/frontend/app/api/startup/route.ts
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to force-static
 
-export async function GET(request: NextRequest) {
+export async function POST() {
   return NextResponse.json("api started");
 }
