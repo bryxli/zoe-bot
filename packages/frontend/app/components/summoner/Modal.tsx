e0f3b0f9a50dbc56ada168d15d5620fa21ffabdc
bryxli <bryanzero765@gmail.com>
2023-12-10 17:49:23 -0600

    summoners prop type

--- packages/frontend/app/components/summoner/Modal.tsx
+++ packages/frontend/app/components/summoner/Modal.tsx
@@ -5,7 +5,7 @@ import { SummonerModalProps } from "../../types";
 export default function SummonerModal({
   showModal,
   onHide,
-  name,
+  summoner,
 }: SummonerModalProps) {
   const deleteUser = () => {
     console.log("deluser");
@@ -17,7 +17,7 @@ export default function SummonerModal({
         <Container fluid>
           <Row>
             <Col>
-              <Modal.Title>{name}</Modal.Title>
+              <Modal.Title>{summoner.name}</Modal.Title>
             </Col>
           </Row>
         </Container>
