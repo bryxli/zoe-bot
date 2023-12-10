72efbbc55a2b48bfe86eef0761b1e3ccba040bd4
bryxli <bryanzero765@gmail.com>
2023-12-09 23:13:20 -0600

    fullscreen modal

--- packages/frontend/app/components/GuildModal.tsx
+++ packages/frontend/app/components/GuildModal.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container, Modal, Row } from "react-bootstrap";
+import { Col, Container, Modal, Row } from "react-bootstrap";
 import Image from "next/image";
 
 import { GuildModalProps } from "../types";
@@ -39,28 +39,40 @@ export default function GuildModal({
   }, [id]);
 
   return (
-    <Modal show={showModal} onHide={onHide}>
+    <Modal show={showModal} onHide={onHide} fullscreen={true}>
       <Modal.Header closeButton>
-        <Modal.Title>
-          {name}
-          {icon && (
-            <Image
-              src={`https://cdn.discordapp.com/icons/${id}/${icon}.jpg`}
-              alt="Guild Avatar"
-              width={30}
-              height={30}
-              priority={true}
-            />
-          )}
-        </Modal.Title>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Modal.Title>{name}</Modal.Title>
+            </Col>
+            {icon && (
+              <Col xs="auto">
+                <Image
+                  src={`https://cdn.discordapp.com/icons/${id}/${icon}.jpg`}
+                  alt="Guild Avatar"
+                  width={40}
+                  height={40}
+                  priority={true}
+                />
+              </Col>
+            )}
+          </Row>
+        </Container>
       </Modal.Header>
       <Modal.Body>
-        <Container>
-          {userlist.length > 0
-            ? userlist.map((user) => {
-                return <Row key={user}>{user}</Row>;
-              })
-            : "userlist is empty"}
+        <Container fluid>
+          {userlist.length > 0 ? (
+            userlist.map((user) => {
+              return (
+                <Row className="mx-auto" ml-2 key={user}>
+                  {user}
+                </Row>
+              );
+            })
+          ) : (
+            <Row className="mx-auto">userlist is empty</Row>
+          )}
         </Container>
       </Modal.Body>
     </Modal>
