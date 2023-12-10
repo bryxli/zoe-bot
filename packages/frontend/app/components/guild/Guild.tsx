a7af77830e8f4e5b94ada19968f4bafa29f8212c
bryxli <bryanzero765@gmail.com>
2023-12-10 15:25:10 -0600

    fill out guild info

--- packages/frontend/app/components/guild/Guild.tsx
+++ packages/frontend/app/components/guild/Guild.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { DynamoGuildProps, GuildProps } from "../../types";
--- packages/frontend/app/components/guild/Information.tsx
+++ packages/frontend/app/components/guild/Information.tsx
@@ -3,11 +3,20 @@ import { useEffect, useState } from "react";
 import { Card, Row } from "react-bootstrap";
 
 export default function GuildInfo(guild: DynamoGuildProps) {
+  const [setup, setSetup] = useState(false);
+  const [webhookLocation, setWebhookLocation] = useState("");
+  const [region, setRegion] = useState("guild needs to be setup");
   const [acknowledged, setAcknowledged] = useState(false);
 
   useEffect(() => {
+    setSetup(guild.guild_id !== "" && guild.guild_id !== undefined);
     setAcknowledged(guild.acknowledgment || false);
   }, [guild]);
+
+  useEffect(() => {
+    // get webhook location using webhook id
+    setup && setRegion(guild.region);
+  }, [guild, setup]);
 
   return (
     <Card>
@@ -15,14 +24,16 @@ export default function GuildInfo(guild: DynamoGuildProps) {
         <Card.Title>guild info</Card.Title>
       </Card.Header>
       <Card.Body>
-        <Row className="mx-auto">setup status</Row>
+        <Row className="mx-auto">
+          setup status: {setup !== undefined && setup.toString()}
+        </Row>
+        <Row className="mx-auto">current webhook location: </Row>
+        <Row className="mx-auto">current region location: {region}</Row>
         <Row className="mx-auto">
           acknowledgement status:{" "}
           {acknowledged !== undefined && acknowledged.toString()}
         </Row>
-        <Row className="mx-auto">current webhook location</Row>
-        <Row className="mx-auto">current region location</Row>
-        <Row className="mx-auto">number of registered players</Row>
+        <Row className="mx-auto">number of registered players: </Row>
       </Card.Body>
     </Card>
   );
