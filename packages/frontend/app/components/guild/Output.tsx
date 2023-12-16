fe336ee825b250b9f9b865d6767c16e56e408a85
bryxli <bryanzero765@gmail.com>
2023-12-16 12:04:02 -0600

    output component

---
+++ packages/frontend/app/components/guild/Output.tsx
@@ -0,0 +1,14 @@
+import { Card } from "react-bootstrap";
+
+import { UserListProps } from "@/app/types";
+
+export default function Output() {
+  return (
+    <Card className="h-100">
+      <Card.Header>
+        <Card.Title>output</Card.Title>
+      </Card.Header>
+      <Card.Body>test</Card.Body>
+    </Card>
+  );
+}
