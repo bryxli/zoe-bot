b034a292927a14aa3eb611df3417f5c52aef8885
bryxli <bryanzero765@gmail.com>
2024-05-22 00:21:35 -0500

    refactor argument parser

--- packages/functions/src/main/argument_parser.py
+++ packages/functions/src/main/argument_parser.py
@@ -1,10 +1,9 @@
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--local", type=bool, default=False)
+
+args = parser.parse_args()
+
 def get_local_status():
-    try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--local", type=bool, default=False)
-        parser.parse_args()
-        return True
-    except:
-        return False
+    return args.local
