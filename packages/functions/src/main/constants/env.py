81331f271374a1d4864b00430dc2b122ca9e6494
bryxli <bryanzero765@gmail.com>
2024-05-22 00:10:16 -0500

    test get local status

--- packages/functions/src/main/constants/env.py
+++ packages/functions/src/main/constants/env.py
@@ -1,16 +1,7 @@
 import os
 
-from argument_parser import get_local_status
-
-if get_local_status():
-    AWS_REGION = "config"
-    DISCORD_PUBLIC_KEY = "config"
-    RIOT_KEY = "config"
-    TOKEN = "config"
-    STAGE = "config"
-else:
-    AWS_REGION = os.environ.get("SET_AWS_REGION")
-    DISCORD_PUBLIC_KEY = os.environ.get("DISCORD_PUBLIC_KEY")
-    RIOT_KEY = os.environ.get("RIOT_KEY")
-    TOKEN = os.environ.get("TOKEN")
-    STAGE = os.environ.get("STAGE")
+AWS_REGION = os.environ.get("SET_AWS_REGION")
+DISCORD_PUBLIC_KEY = os.environ.get("DISCORD_PUBLIC_KEY")
+RIOT_KEY = os.environ.get("RIOT_KEY")
+TOKEN = os.environ.get("TOKEN")
+STAGE = os.environ.get("STAGE")
