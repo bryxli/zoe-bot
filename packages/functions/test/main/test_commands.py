46cdddd4ae8db7b3eaedaa01cb713190cc8199e2
bryxli <bryanzero765@gmail.com>
2024-05-22 23:04:48 -0500

    starter tests for commands

--- packages/functions/test/main/test_commands.py
+++ packages/functions/test/main/test_commands.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 class TestCommands(unittest.TestCase):
     
@@ -18,35 +18,47 @@ class TestCommands(unittest.TestCase):
         self.league_init = league_init
         self.server_init = server_init
 
+        self.data = {
+            "guild_id": 1,
+            "data": {
+                "options": [
+                    {"value": "foo"}, 
+                    {"value": "bar"}
+                ]
+            }
+        }
+
         sys.path.remove(directory)
 
+    # TODO: tests not fully refined, does not test exceptions, conditions, etc
+
     def test_add_user(self):
-        res = 'test'
-        self.assertEqual(res, 'test')
+        res = self.league_init('adduser', self.data)
+        self.assertEqual(res, 'player registered')
     
     def test_delete_user(self):
-        res = 'test'
-        self.assertEqual(res, 'test')
+        res = self.league_init('deluser', self.data)
+        self.assertEqual(res, 'player deleted')
 
     def test_userlist(self):
-        res = 'test'
-        self.assertEqual(res, 'test')
+        res = self.league_init('userlist', self.data)
+        self.assertEqual(res, 'no users are registered')
 
     def test_init_guild(self):
-        res = 'test'
-        self.assertEqual(res, 'test')
+        res = self.server_init('setup', self.data)
+        self.assertEqual(res, 'guild already exists')
 
     def test_delete_guild(self):
-        res = 'test'
-        self.assertEqual(res, 'test')              
+        res = self.server_init('reset', self.data)
+        self.assertEqual(res, 'guild deleted')            
 
     def test_change_region(self):
-        res = 'test'
-        self.assertEqual(res, 'test')    
+        res = self.server_init('region', self.data)
+        self.assertEqual(res, 'region not found') 
 
     def test_acknowledge(self):
-        res = 'test'
-        self.assertEqual(res, 'test')        
+        res = self.server_init('acknowledge', self.data)
+        self.assertEqual(res, 'successfully acknowledged')  
 
 if __name__ == '__main__':
     unittest.main()
