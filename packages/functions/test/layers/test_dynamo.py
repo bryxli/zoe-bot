df9281685d53f6e9d44f8872dfabbc2e76e95cb0
bryxli <bryanzero765@gmail.com>
2024-05-24 18:19:27 -0500

    boilerplate for dynamo unit test

--- packages/functions/test/layers/test_dynamo.py
+++ packages/functions/test/layers/test_dynamo.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import unittest
-from unittest.mock import MagicMock
 
 class TestDynamoLayer(unittest.TestCase):
     def __init__(self, methodName='runTest') -> None:
@@ -11,13 +10,42 @@ class TestDynamoLayer(unittest.TestCase):
         sys.path.append(directory)
 
         from dynamo import ZoeBotTable
-        self.ZoeBotTable = ZoeBotTable
+
+        self.region = os.environ.get("SET_AWS_REGION")
+        self.stage = "dev"
+        self.db = ZoeBotTable(self.region, self.stage)
 
         sys.path.remove(directory)
 
-    def test_dynamo(self):
-        res = 'test'
-        self.assertEqual(res, 'test')   
+    def test_get_all(self):
+        res = self.db.get_all()
+        self.assertIn("{'Items': ", str(res))
+
+    def test_guild_actions(self):
+        def test_create_guild(self):
+            self.assertTrue(True)
+
+        def test_guild_exists(self):
+            self.assertTrue(True)
+
+        def test_get_guild(self):
+            self.assertTrue(False)
+
+        def test_update_guild(self):
+            self.assertTrue(False)
+
+        test_create_guild(self)
+        test_guild_exists(self)
+        test_get_guild(self)
+        test_update_guild(self)
+        #add_user
+        #get_all_users
+        #user_exists
+        #update_user
+        #delete_user
+        #check_aknowledgment
+        #get_webhook
+        #destroy_guild
 
 if __name__ == '__main__':
     unittest.main()
