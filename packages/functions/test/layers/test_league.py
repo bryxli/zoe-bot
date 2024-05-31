30f28cb0b738eaa909bc3e36d5866d20a3e6d049
bryxli <bryanzero765@gmail.com>
2024-05-30 19:27:26 -0500

    implementation for league tests

--- packages/functions/test/layers/test_league.py
+++ packages/functions/test/layers/test_league.py
@@ -22,26 +22,91 @@ class TestLeagueLayer(unittest.TestCase):
 
         sys.path.remove(directory)
 
-    def test_get_AccountDto_by_riot_id():
-        pass
+    def test_league_actions(self):
+        def test_get_AccountDto_by_riot_id():
+            res = self.RiotAPI.get_AccountDto_by_riot_id('bryxli', 'NA1', 'NA')
+            self.assertIn("'gameName': 'bryxli', 'tagLine': 'NA1'}", str(res))
+            return res
 
-    def test_get_AccountDto_by_puuid():
-        pass
+        def test_get_AccountDto_by_puuid(puuid):
+            res = self.RiotAPI.get_AccountDto_by_puuid(puuid, 'NA')
+            self.assertIn("'gameName': 'bryxli', 'tagLine': 'NA1'}", str(res))
 
-    def test_get_matchId_by_puuid():
-        pass
+        def test_get_matchId_by_puuid(puuid):
+            res = self.RiotAPI.get_matchId_by_puuid(puuid, 'NA')
+            if not res.startswith('NA1_'):
+                self.assertTrue(False)
+            return res
 
-    def test_get_MatchDto_by_matchId():
-        pass
+        def test_get_MatchDto_by_matchId(matchId):
+            res = self.RiotAPI.get_MatchDto_by_matchId(matchId, 'NA')
+            self.assertIn(f"'matchId': '{matchId}'", str(res))
 
-    def test_get_puuid_by_riot_id():
-        pass
+        def test_get_AccountDto_by_riot_id_error():
+            try:
+                self.RiotAPI.get_AccountDto_by_riot_id('GN', 'NA1', 'NA')
+            except:
+                pass
+            else:
+                self.assertTrue(False)
 
-    def test_get_name_by_puuid():
-        pass
+        def test_get_AccountDto_by_puuid_error():
+            try:
+                self.RiotAPI.get_AccountDto_by_puuid('foo', 'NA')
+            except:
+                pass
+            else:
+                self.assertTrue(False)
 
-    def test_get_match_by_puuid():
-        pass
+        def test_get_matchId_by_puuid_error():
+            try:
+                self.RiotAPI.get_matchId_by_puuid('foo', 'NA')
+            except:
+                pass
+            else:
+                self.assertTrue(False)
+
+        def test_get_MatchDto_by_matchId_error():
+            try:
+                self.RiotAPI.get_MatchDto_by_matchId('foo', 'NA')
+            except:
+                pass
+            else:
+                self.assertTrue(False)
+
+        def test_get_puuid_by_riot_id():
+            try:
+                puuid = self.RiotAPI.get_puuid_by_riot_id('bryxli', 'NA1', 'NA')
+            except:
+                self.assertTrue(False)
+            return puuid
+
+        def test_get_name_by_puuid(puuid):
+            try:
+                self.RiotAPI.get_name_by_puuid(puuid, 'NA')
+            except:
+                self.assertTrue(False)
+
+        def test_get_match_by_puuid(puuid):
+            try:
+                self.RiotAPI.get_match_by_puuid(puuid, 'NA')
+            except:
+                self.assertTrue(False)
+
+        test_get_AccountDto_by_riot_id()['puuid']
+        puuid = test_get_puuid_by_riot_id()
+
+        matchId = test_get_matchId_by_puuid(puuid)
+        test_get_MatchDto_by_matchId(matchId)
+
+        test_get_AccountDto_by_puuid(puuid)
+        test_get_name_by_puuid(puuid)
+        test_get_match_by_puuid(puuid)
+
+        test_get_AccountDto_by_riot_id_error()
+        test_get_AccountDto_by_puuid_error()
+        test_get_matchId_by_puuid_error()
+        test_get_MatchDto_by_matchId_error()
 
 if __name__ == '__main__':
     unittest.main()
