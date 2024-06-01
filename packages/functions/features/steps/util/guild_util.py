8f065d7a50e8bb96995d25b7c18a55781bc94719
bryxli <bryanzero765@gmail.com>
2024-06-01 01:22:49 -0500

    rename dynamo_util to guild_util

--- packages/functions/features/steps/util/guild_util.py
+++ packages/functions/features/steps/util/guild_util.py
@@ -2,7 +2,7 @@ import sys
 import os
 import json
 
-class DynamoUtil:
+class GuildUtil:
     def __init__(self):
         directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src/layers/dynamo/python'))
         sys.path.append(directory)
