566856b7ef7105fb828c7c47207140d2722da143
bryxli <bryanzero765@gmail.com>
2024-06-02 02:12:10 -0500

    manually set stage

--- packages/functions/src/main/constants/env.py
+++ packages/functions/src/main/constants/env.py
@@ -4,7 +4,7 @@ import json
 import logging
 logging.basicConfig(level=logging.WARNING)
 
-if os.environ.get("STAGE") is None:
+if os.environ.get("SET_AWS_REGION") is None:
     logging.warning('this should not be ran in actions workflow')
     config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../configs/config.json'))
     with open(config_path, "r") as config_file:
@@ -19,5 +19,11 @@ else:
     DISCORD_PUBLIC_KEY = os.environ.get("DISCORD_PUBLIC_KEY")
     RIOT_KEY = os.environ.get("RIOT_KEY")
     TOKEN = os.environ.get("TOKEN")
-    STAGE = os.environ.get("STAGE")
+    if os.environ.get("STAGE") == None:
+        STAGE = "dev"
+    else:
+        STAGE = os.environ.get("STAGE")
+    logging.warning(f'env region: {AWS_REGION}')
+    logging.warning(f'env public key: {DISCORD_PUBLIC_KEY}')
+    logging.warning(f'env token: {TOKEN}')
     logging.warning(f'env stage: {STAGE}')
