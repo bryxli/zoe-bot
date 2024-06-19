ffce1549a54f723adb08e77eea855eb75ad2e782
bryxli <bryanzero765@gmail.com>
2024-06-19 15:55:27 -0500

    remove argument_parser.py

--- packages/functions/src/main/argument_parser.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--local", type=bool, default=False)
-
-args = parser.parse_args()
-
-def get_local_status():
-    return args.local
