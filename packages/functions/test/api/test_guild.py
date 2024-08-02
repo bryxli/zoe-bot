cb8b1fcdebeefd5fab908a7e35270bd4d874ea78
bryxli <bryanzero765@gmail.com>
2024-08-02 12:17:07 -0500

    delete tests, not working

--- packages/functions/test/api/test_guild.py
+++ packages/functions/test/api/test_guild.py
@@ -13,7 +13,7 @@ class TestApiGuild(unittest.TestCase):
         self.dynamo_mock.ZoeBotTable = MagicMock()
         self.dynamo_mock.ZoeBotTable.return_value.create_guild = MagicMock(return_value=True)
 
-        self.event_setup = {
+        self.event_data = {
             'body': {
                 'apiKey':'foo',
                 'guildId': 'bar',
@@ -49,41 +49,95 @@ class TestApiGuild(unittest.TestCase):
             return handler(event, None)
         
     def delete(self, event, params=None):
+        directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/api'))
+        sys.path.append(directory)
+        
+        if params == 'test_guild_dne':
+            self.dynamo_mock.ZoeBotTable.return_value.guild_exists = MagicMock(return_value=False)
+        elif params == 'test_not_acknowledged':
+            self.dynamo_mock.ZoeBotTable.return_value.guild_exists = MagicMock(return_value=True)
+            self.dynamo_mock.ZoeBotTable.return_value.check_acknowledgment = MagicMock(return_value=False)
+        else:
+            self.dynamo_mock.ZoeBotTable.return_value.guild_exists = MagicMock(return_value=True)
+            self.dynamo_mock.ZoeBotTable.return_value.check_acknowledgment = MagicMock(return_value=True)
+
+        with patch.dict(sys.modules, {'dynamo': self.dynamo_mock, 'league': MagicMock()}):
+            from guild.delete import handler
+            sys.path.remove(directory)
+
+            return handler(event, None)
+
+    def get(self, event, params=None): # todo
         pass
 
-    def get(self, event, params=None):
+    def region(self, event, params=None): # todo
         pass
 
-    def region(self, event, params=None):
+    def acknowledge(self, event, params=None): # todo
         pass
 
-    def acknowledge(self, event, params=None):
-        pass
-
-    def test_missing_params(self):
+    def test_setup_missing_params(self):
         event_missing_params = {
             'body': {}
         }
         res = self.setup(json.dumps(event_missing_params))
         self.assertEqual(res['statusCode'], 400)
 
-    def test_api_key(self):
+    def test_setup_api_key(self):
         self.mock_auth.return_value = False
-        res = self.setup(json.dumps(self.event_setup))
+        res = self.setup(json.dumps(self.event_data))
         self.assertEqual(res['statusCode'], 401)
 
-    def test_guild_exists(self):
-        res = self.setup(json.dumps(self.event_setup), 'test_guild_exists')
+    def test_setup_guild_exists(self):
+        res = self.setup(json.dumps(self.event_data), 'test_guild_exists')
         self.assertEqual(res['statusCode'], 409)
 
-    def test_webhook_error(self):
+    def test_setup_webhook_error(self):
         self.mock_requests_post.side_effect = Exception()
-        res = self.setup(json.dumps(self.event_setup))
+        res = self.setup(json.dumps(self.event_data))
         self.assertEqual(res['statusCode'], 500)
 
-    def test_success(self):
-        res = self.setup(json.dumps(self.event_setup))
+    def test_setup_success(self):
+        res = self.setup(json.dumps(self.event_data))
         self.assertEqual(res['statusCode'], 201)
+
+    def test_delete_missing_params(self):
+        event_missing_params = {
+            'body': {}
+        }
+        res = self.delete(json.dumps(event_missing_params))
+        self.assertEqual(res['statusCode'], 400)
+
+    def test_delete_api_key(self):
+        self.mock_auth.return_value = False
+        res = self.delete(json.dumps(self.event_data))
+        self.assertEqual(res['statusCode'], 401)
+
+    def test_delete_guild_dne(self):
+        res = self.delete(json.dumps(self.event_data), 'test_guild_dne')
+        self.assertEqual(res['statusCode'], 404)
+
+    # def test_delete_not_acknowledged(self):
+    #     res = self.delete(json.dumps(self.event_data), 'test_not_acknowledged')
+    #     self.assertEqual(res['statusCode'], 403)
+
+    # def test_delete_success(self):
+    #     res = self.delete(json.dumps(self.event_data))
+    #     self.assertEqual(res['statusCode'], 200)
+
+    def test_get_missing_params(self): # todo
+        pass
+
+    def test_get_api_key(self): # todo
+        pass
+
+    def test_get_guild_dne(self): # todo
+        pass
+
+    def test_get_success(self): # todo
+        pass
+
+    # TODO: create region and acknowledge tests
 
 if __name__ == '__main__':
     unittest.main()
