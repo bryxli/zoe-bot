e96026df0a64e32c280bcac3ed1b52b8f1d58909
bryxli <bryanzero765@gmail.com>
2024-08-01 23:44:35 -0500

    move mock to init

--- packages/functions/test/api/test_guild.py
+++ packages/functions/test/api/test_guild.py
@@ -9,6 +9,10 @@ class TestApiGuild(unittest.TestCase):
     def __init__(self, methodName='runTest') -> None:
         super().__init__(methodName)
 
+        self.dynamo_mock = MagicMock()
+        self.dynamo_mock.ZoeBotTable = MagicMock()
+        self.dynamo_mock.ZoeBotTable.return_value.create_guild = MagicMock(return_value=True)
+
         self.event_setup = {
             'body': {
                 'apiKey':'foo',
@@ -32,21 +36,29 @@ class TestApiGuild(unittest.TestCase):
     def setup(self, event, params=None):
         directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src/api'))
         sys.path.append(directory)
-
-        dynamo_mock = MagicMock()
-        dynamo_mock.ZoeBotTable = MagicMock()
-        dynamo_mock.ZoeBotTable.return_value.create_guild = MagicMock(return_value=True)
-
+        
         if params == 'test_guild_exists':
-            dynamo_mock.ZoeBotTable.return_value.guild_exists = MagicMock(return_value=True)
+            self.dynamo_mock.ZoeBotTable.return_value.guild_exists = MagicMock(return_value=True)
         else:
-            dynamo_mock.ZoeBotTable.return_value.guild_exists = MagicMock(return_value=False)
+            self.dynamo_mock.ZoeBotTable.return_value.guild_exists = MagicMock(return_value=False)
 
-        with patch.dict(sys.modules, {'dynamo': dynamo_mock, 'league': MagicMock()}):
+        with patch.dict(sys.modules, {'dynamo': self.dynamo_mock, 'league': MagicMock()}):
             from guild.setup import handler
-
             sys.path.remove(directory)
+
             return handler(event, None)
+        
+    def delete(self, event, params=None):
+        pass
+
+    def get(self, event, params=None):
+        pass
+
+    def region(self, event, params=None):
+        pass
+
+    def acknowledge(self, event, params=None):
+        pass
 
     def test_missing_params(self):
         event_missing_params = {
